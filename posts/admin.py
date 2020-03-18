f4c84ad684d145ba78d40337d9272af34cd8365b
Alexis Araujo <ala1289@gmail.com>
2020-03-18 16:16:37 -0600

    Templates y archivos estáticos

--- posts/admin.py
+++ posts/admin.py
@@ -8,8 +8,8 @@ from posts.models import Post
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('pk', 'user', 'title', 'photo',)
-    list_display_links = ('pk', 'user',)
-    list_editable = ('title', 'photo',)
-    search_fields = ('user__username', 'title',)
-    readonly_fields = ('created', 'modified')
+    """Post admin."""
+
+    list_display = ('id', 'user', 'title', 'photo')
+    search_fields = ('title', 'user__username', 'user__email')
+    list_filter = ('created', 'modified')
