e6932098ebdb6ad7a23e3c856fc053d692ed9a69
Alexis Araujo <ala1289@gmail.com>
2020-03-18 19:04:11 -0600

    Middlewares

--- users/views.py
+++ users/views.py
@@ -13,6 +13,12 @@ from django.contrib.auth.models import User
 from users.models import Profile
 
 
+@login_required
+def update_profile(request):
+    """Update a user's profile view."""
+    return render(request, 'users/update_profile.html')
+
+
 def login_view(request):
     """Login view."""
     if request.method == 'POST':
