88852ff2fb5f6a00c6ccf90e74dc1ccaff2a4e05
Alexis Araujo <ala1289@gmail.com>
2020-03-20 10:14:36 -0600

    CreateView, FormView y UpdateView

--- users/forms.py
+++ users/forms.py
@@ -56,10 +56,12 @@ class SignupForm(forms.Form):
         profile.save()
 
 
-class ProfileForm(forms.Form):
-    """Profile form."""
-
-    website = forms.URLField(max_length=200, required=True)
-    biography = forms.CharField(max_length=500, required=False)
+class ProfileForm(forms.ModelForm):
+    website = forms.URLField(max_length=200, required=False)
+    biography = forms.CharField(max_length=500, required=True)
     phone_number = forms.CharField(max_length=20, required=False)
-    picture = forms.ImageField(required=False)
+    picture = forms.ImageField(required=True)
+
+    class Meta:
+        model = Profile
+        fields = ['website', 'biography', 'phone_number', 'picture']
